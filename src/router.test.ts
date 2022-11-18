92c1e63b7cebebc8a88fddf449ad5221797d6cf8
Michel Smola <michel.smola@farbenmeer.de>
2022-11-18 14:05:23 +0100

    setup jest

---
+++ src/router.test.ts
@@ -0,0 +1,7 @@
+import { generateRouter } from "./router";
+
+describe("router", () => {
+  it("routes", async () => {
+    await generateRouter();
+  });
+});
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,41 @@
 import fs from "fs/promises";
 
-export function generateRouter() {
+export async function generateRouter() {
+  async function parseDirectory(path: string, root: any) {
+    const routes = await fs.readdir(path);
+
+    for (const entry of routes) {
+      if (entry === "route.ts") {
+        root.index = true;
+        return;
+      }
+
+      if ((await fs.stat(path + "/" + entry)).isDirectory()) {
+        root[entry] = {};
+        await parseDirectory(path + "/" + entry, tree[entry]);
+      }
+    }
+  }
+
+  const tree: any = {};
+  await parseDirectory("examples/todo/routes", tree);
+
+  console.log({ tree });
   // read routes directory
   // figure out routes
   // build imports for get-function from each route
   // correctly handle requests
-  console.log(`
-  import * as homeRoute from "routes/route.ts"
-  import * as contactRoute from "routes/contact/route.ts"
-
-  export default function router(req: Request) {
-      const url = new URL(req.url)
-      const segments = url.pathname.split('/')
-
-      // code that finally calls homeRoute or contactRoute
-
-      return new Response(homeRoute.get(req) or contactRoute.get(req))
-  }
-`);
+  //  console.log(`
+  //  import * as homeRoute from "routes/route.ts"
+  //  import * as contactRoute from "routes/contact/route.ts"
+  //
+  //  export default function router(req: Request) {
+  //      const url = new URL(req.url)
+  //      const segments = url.pathname.split('/')
+  //
+  //      // code that finally calls homeRoute or contactRoute
+  //
+  //      return new Response(homeRoute.get(req) or contactRoute.get(req))
+  //  }
+  //`);
 }
