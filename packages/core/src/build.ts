8b54130e2eec07f61ea83c6e6343271f2b7c9366
Michel Smola <michel.smola@farbenmeer.de>
2022-11-19 20:01:06 +0100

    monorepo-setup

--- packages/core/src/build.ts
+++ packages/core/src/build.ts
@@ -4,15 +4,24 @@ import webpackServerConfig from "../webpack/server.webpack.config";
 import webpackWorkerConfig from "../webpack/worker.webpack.config";
 import { webpack } from "webpack";
 import { getServerRuntime } from "./server-runtime";
+import * as path from "path";
 
 export async function build() {
+  const pocketDir = path.resolve(process.cwd(), ".pocket");
+  const routesDir = path.resolve(process.cwd(), "routes");
+
+  try {
+    await fs.stat(pocketDir);
+  } catch {
+    await fs.mkdir(pocketDir);
+  }
   const code = await generateRouter({ server: false });
-  await fs.writeFile(".pocket/worker-entry.js", code);
-  await fs.cp("examples/todo/routes", ".pocket/routes", { recursive: true });
+  await fs.writeFile(path.resolve(pocketDir, "worker-entry.js"), code);
+  await fs.cp(routesDir, path.resolve(pocketDir, "routes"), {
+    recursive: true,
+  });
   await new Promise((resolve, reject) => {
     webpack(webpackWorkerConfig(), (error, stats) => {
-      console.log("compiled", error, stats?.hasErrors());
-
       if (error) {
         return reject(error);
       }
@@ -32,7 +41,7 @@ export async function build() {
   });
 
   const serverCode = await generateRouter({ server: true });
-  await fs.writeFile(".pocket/server-entry.js", serverCode);
+  await fs.writeFile(path.resolve(pocketDir, "server-entry.js"), serverCode);
   await new Promise((resolve, reject) => {
     webpack(webpackServerConfig(), (error, stats) => {
       if (error) {
--- packages/core/src/router.ts
+++ packages/core/src/router.ts
@@ -1,37 +1,38 @@
-import fs from "fs/promises";
+import * as fs from "fs/promises";
+import * as path from "path";
 
 export async function generateRouter(options: { server: boolean }) {
-  const basePath = "examples/todo/routes/";
+  const basePath = path.resolve(process.cwd(), "routes");
   const routes: string[] = [];
 
-  async function parseDirectory(path: string) {
-    const entries = await fs.readdir(basePath + path);
+  async function parseDirectory(dir: string) {
+    const entries = await fs.readdir(path.resolve(basePath, dir));
 
     for (const entry of entries) {
       if (entry === "route.ts") {
-        routes.push(path);
+        routes.push(dir);
         return;
       }
 
-      if ((await fs.stat(basePath + path + "/" + entry)).isDirectory()) {
-        await parseDirectory(path + "/" + entry);
+      if ((await fs.stat(path.resolve(basePath, dir, entry))).isDirectory()) {
+        await parseDirectory(dir + "/" + entry);
       }
     }
   }
 
-  await parseDirectory("");
+  await parseDirectory(".");
 
   const imports = routes.map(
     (route) => `
-    import * as ${
-      route === "" ? "index" : route.replace("/", "_")
-    } from "./routes${route}/route.ts";`
+    import * as ${handlerName(route)} from "./routes${route.slice(
+      1
+    )}/route.ts";`
   );
 
   const handlers = routes.map(
     (route) => `
     if ("${route === "" ? "/" : route}" === url.pathname) {
-        return ${route === "" ? "index" : route.replace("/", "_")}.get(req);
+        return ${handlerName(route)}.get(req);
     }`
   );
 
@@ -45,7 +46,6 @@ export async function generateRouter(options: { server: boolean }) {
   })();
   `;
 
-  console.log({ routes, imports });
   return `
     ${imports.join("\n")}
 
@@ -83,4 +83,6 @@ export async function generateRouter(options: { server: boolean }) {
   `;
 }
 
-// <script src="_pocket/worker.js"></script>
+function handlerName(route: string) {
+  return route.replace(".", "index").replace("/", "_") + "Handler";
+}
